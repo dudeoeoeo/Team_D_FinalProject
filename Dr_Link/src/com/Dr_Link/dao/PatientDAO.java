5fdc48cbd45f8b0393510d62cfa85ccf80d2e848
dudeoeoeo <dudekekek@naver.com>
2020-12-06 21:19:08 +0900

    1206_HY_Final_Start

---
+++ Dr_Link/src/com/Dr_Link/dao/PatientDAO.java
@@ -0,0 +1,8 @@
+package com.Dr_Link.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientDAO {
+	
+}
---
+++ Dr_Link/src/com/Dr_Link/interceptor/Dr_linkInterceptor.java
@@ -0,0 +1,25 @@
+package com.Dr_Link.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class Dr_linkInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("preeeeeeeeeeeee" + request.getParameter("page"));
+		System.out.println("login 페이지 요청");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("afterrrrrrrrrrrrrr");
+	}
+	
+}
