4064ecc372be9e728c2e66fe07ac08afc8a0acc7
dayu97 <dayu1997@kakao.com>
2020-12-20 12:09:24 +0900

    1220_다유

--- Dr_Link/src/kr/co/Dr_Link/mvc/controller/PatientController.java
+++ Dr_Link/src/kr/co/Dr_Link/mvc/controller/PatientController.java
@@ -1,5 +1,7 @@
 package kr.co.Dr_Link.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -16,6 +18,8 @@ import kr.co.Dr_Link.mvc.dao.PrescriptionDaoInter;
 import kr.co.Dr_Link.mvc.dto.DrLinkDTO;
 import kr.co.Dr_Link.mvc.dto.PrescriptionDTO;
 import kr.co.Dr_Link.mvc.dto.TreatmentRecordDTO;
+
+
 
 @Controller
 @RequestMapping(value = "/patients")
@@ -34,28 +38,28 @@ public class PatientController {
 	/* 김다유 : patient_dashboard 페이지로 이동 - 처방기록리스트 */
 	/* patient_dashboard에서 진료기록, 결제기록, 예약기록 담당하시는 분들 여기서 값 세팅해주세요 */
 	@RequestMapping(value = "patient_dashboard")
-	public String treatmentRecord(TreatmentRecordDTO vo, Model model) {
-		List<TreatmentRecordDTO> treatmentRecord = patient_dao.treatmentRecord(vo);
-		model.addAttribute("treatmentRecord", treatmentRecord);
+	public String treatmentRecord(PrescriptionDTO pre_vo, Model model) {
+		try {
+		List<PrescriptionDTO> prescriptionRecord = patient_dao.prescriptionRecord(pre_vo);
+		System.out.println(prescriptionRecord.get(0).getPrescription_date());
+		System.out.println("***"+prescriptionRecord.get(0).getDoctor_num());
+		model.addAttribute("prescriptionRecord", prescriptionRecord);
+		} catch (NullPointerException e) {
+				  
+		}
 		return "/patients/patient_dashboard";
 	}
 	
 	/* 김다유 : 처방기록 상세 페이지로 이동 */
 	@RequestMapping(value = "/detail_prescription") 
 	public String end_prescription(PrescriptionDTO vo,Model model, DrLinkDTO drLinkVo) {
-
 		PrescriptionDTO prescription = pre_dao.detail_prescription(vo);
 		DrLinkDTO drLinkinfo = pre_dao.prescription_info(drLinkVo);
 		
-		String[] dosage = prescription.getDosage();
-		String[] quantity = prescription.getQuantity();
-		String[] taking_Date = prescription.getTaking_date();
-		String[] medicine_num = prescription.getMedicine_num(); 
-		System.out.println("배열길이: "+ medicine_num.length);
-		for (int i = 0 ; i < medicine_num.length; i++) {
-			System.out.println("약번호 : "+medicine_num[i]+"/"+taking_Date[i]+"일/"+dosage[i]+"알/"+quantity[i]+"회");
-		}
-
+		System.out.println("★getPatient_num : "+ prescription.getPatient_num());
+		System.out.println("★getDoctor_num : "+ prescription.getDoctor_num());
+		System.out.println("★getTreatment_num : "+ prescription.getTreatment_num());
+		
 		model.addAttribute("prescription",prescription);
 		model.addAttribute("drLinkinfo",drLinkinfo);
 		System.out.println("controller detail_prescription 실행 완료");
