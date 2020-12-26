77c40fba69eaf34fc9292033a7d1d916d065c576
dayu97 <dayu1997@kakao.com>
2020-12-26 16:55:36 +0900

    1226_다유 prescription dao 수정

--- Dr_Link/src/kr/co/Dr_Link/mvc/dao/PrescriptionDaoImp.java
+++ Dr_Link/src/kr/co/Dr_Link/mvc/dao/PrescriptionDaoImp.java
@@ -12,11 +12,19 @@ import kr.co.Dr_Link.mvc.dto.MedicineDTO;
 import kr.co.Dr_Link.mvc.dto.PatientDTO;
 import kr.co.Dr_Link.mvc.dto.PrescriptionDTO;
 
+
 @Repository("PrescriptionDaoInter")
 public class PrescriptionDaoImp implements PrescriptionDaoInter {
 	
 	@Autowired
 	private SqlSessionTemplate ss;
+	
+	/* 김다유 처방기록 select */
+	@Override
+	public List<PrescriptionDTO> prescriptionRecord(int patient_num) {
+		System.out.println("===> Mybatis prescriptionRecord() 실행");
+		return ss.selectList("prescription.prescriptionRecord", patient_num);
+	}
 	
 	/* 리스트 처리 해야 함 ... */
 	@Override
@@ -55,7 +63,7 @@ public class PrescriptionDaoImp implements PrescriptionDaoInter {
 		System.out.println("===> Mybatis medicine_detail_info() 실행");
 		ArrayList<MedicineDTO> medi_num = new ArrayList<MedicineDTO>();
 		for(String medicine : medicine_num) {
-			medi_num.add(ss.selectOne("prescription.medicine_detail_info",medicine));
+			medi_num.add((MedicineDTO)ss.selectOne("prescription.medicine_detail_info",medicine));
 		}
 		return medi_num;
 	}
--- Dr_Link/src/kr/co/Dr_Link/mvc/dao/PrescriptionDaoInter.java
+++ Dr_Link/src/kr/co/Dr_Link/mvc/dao/PrescriptionDaoInter.java
@@ -7,7 +7,11 @@ import kr.co.Dr_Link.mvc.dto.MedicineDTO;
 import kr.co.Dr_Link.mvc.dto.PatientDTO;
 import kr.co.Dr_Link.mvc.dto.PrescriptionDTO;
 
+
+
 public interface PrescriptionDaoInter {
+	/* public DoctorDTO detail_prescription(DoctorDTO vo); */
+	public List<PrescriptionDTO> prescriptionRecord (int patient_num);
 	public void add_prescription(PrescriptionDTO vo);
 	public PrescriptionDTO detail_prescription(PrescriptionDTO vo) ;
 	public PatientDTO patient_info(int patient_num);
